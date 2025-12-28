ebbb0a700120a26bdab4936120690fd470921239
Sahil Ahmed <sahilahmed466@gmail.com>
2025-12-28 23:53:16 +0530

    Create and Organize the CRUD routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import remindersRoutes from './routes/remindersRoutes.js';
+import usersRoutes from './routes/usersRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+app.use('/reminders', remindersRoutes);
+app.use('/users', usersRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
